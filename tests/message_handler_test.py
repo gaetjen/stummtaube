1bfb5d7de2563b13bdd9ec3ea3e3868a10319ac2
Johannes Gätjen <johannesgaetjen@gmail.com>
2020-12-05 12:22:23 +0100

    test: fix tests for async stuff

--- tests/message_handler_test.py
+++ tests/message_handler_test.py
@@ -1,70 +1,84 @@
-from unittest.mock import Mock, PropertyMock, patch, MagicMock
+from unittest.mock import Mock, PropertyMock, patch
 
+import pytest
 from discord import TextChannel, DMChannel
 
+from stummtaube.main import client
 from stummtaube.message_handler import handle_message
 
 
-def test_text_channel_should_not_be_processed():
+@pytest.mark.asyncio
+async def test_text_channel_should_not_be_processed():
     mock_message = Mock()
     channel_mock = PropertyMock(TextChannel)
     type(mock_message).channel = channel_mock
     content_mock = PropertyMock()
     type(mock_message).content = content_mock
 
-    handle_message(mock_message)
+    await handle_message(mock_message)
 
     content_mock.assert_not_called()
 
 
-@patch("stummtaube.message_handler.players")
-def test_join_adds_user(mocked_players):
-    mocked_players.add = MagicMock(name="add to players")
+@pytest.mark.asyncio
+async def test_messages_from_bot_should_not_be_processed():
+    mock_message = Mock()
+    mock_message.author = client
+    type(mock_message).channel = PropertyMock(DMChannel)
+    content_mock = PropertyMock()
+    type(mock_message).content = content_mock
+
+    await handle_message(mock_message)
+
+    content_mock.assert_not_called()
+
+
+@pytest.mark.asyncio
+@patch("stummtaube.message_handler.join_player")
+async def test_join_adds_user(join_player):
     mock_message = Mock(name="message")
     mock_message.channel = PropertyMock(DMChannel, name="channel")
     mock_message.content = "!join"
-    author_mock = PropertyMock(name="author")
-    type(mock_message).author = author_mock
 
-    handle_message(mock_message)
+    await handle_message(mock_message)
 
-    author_mock.assert_called()
-    mocked_players.add.assert_called_with(mock_message.author)
+    join_player.assert_called_with(mock_message.author)
 
 
-@patch("stummtaube.message_handler.rounds")
-def test_start_does_not_start_round_when_player_is_not_joined(mocked_rounds):
+@pytest.mark.asyncio
+@patch("stummtaube.message_handler.create_round")
+async def test_start_does_not_start_round_when_player_is_not_joined(mock_create):
     mock_message = Mock(name="message")
     mock_message.channel = PropertyMock(DMChannel, name="channel")
     mock_message.content = "!start Grünes Zebra"
 
-    handle_message(mock_message)
+    await handle_message(mock_message)
 
-    assert len(mocked_rounds.method_calls) == 0
+    mock_create.assert_not_called()
 
 
-@patch("stummtaube.message_handler.Round", MagicMock())
-@patch("stummtaube.message_handler.rounds")
-def test_start_creates_round(mocked_rounds):
-    mocked_rounds.append = MagicMock()
+@patch("stummtaube.message_handler.create_round")
+@pytest.mark.asyncio
+async def test_start_creates_round(mock_create):
     mock_message = Mock(name="message")
     mock_message.channel = PropertyMock(DMChannel, name="channel")
     mock_message.content = "!start Grünes Zebra"
 
     with patch("stummtaube.message_handler.players", {mock_message.author}):
-        handle_message(mock_message)
-        mocked_rounds.append.assert_called()
+        await handle_message(mock_message)
+        mock_create.assert_called_with(mock_message)
 
 
-@patch("stummtaube.message_handler.players")
-@patch("stummtaube.message_handler.rounds")
-def test_no_command_falls_through(mocked_rounds, mocked_players):
+@patch("stummtaube.message_handler.join_player")
+@patch("stummtaube.message_handler.create_round")
+@pytest.mark.asyncio
+async def test_no_command_falls_through(mock_create, join_player):
     mock_message = Mock(name="message")
     mock_message.channel = PropertyMock(DMChannel, name="channel")
     mock_message.content = "Ich bin eine Biene!"
 
-    handle_message(mock_message)
+    await handle_message(mock_message)
 
-    assert len(mocked_rounds.method_calls) == 0
-    assert len(mocked_players.method_calls) == 0
+    mock_create.assert_not_called()
+    join_player.assert_not_called()
 
--- tests/round_test.py
+++ tests/round_test.py
@@ -1,15 +1,20 @@
-from unittest.mock import Mock, patch, MagicMock
+from unittest.mock import Mock, patch, MagicMock, AsyncMock
 
-from stummtaube.data.round import Round
+import pytest
+
+from stummtaube.data.round import Round, create_round, rounds
 
 
-def test_created_round_has_message_and_sends_it():
+@pytest.mark.asyncio
+async def test_created_round_has_message_and_sends_it():
     user_mock = MagicMock()
-    user_mock.send = MagicMock()
+    user_mock.send = AsyncMock()
     with patch("stummtaube.data.round.players", {user_mock}):
         message = Mock()
         message.content = "!start Bierschiss"
-        new_round = Round(message)
-        assert message in new_round._messages
-        assert len(new_round._messages) == 1
+
+        await create_round(message)
+
+        assert message in rounds[0]._messages
+        assert len(rounds[0]._messages) == 1
         user_mock.send.assert_called_with("Bierschiss")
